e57ff96f7053724e05525de129cb2900e53a3a52
Md. Reyanus Salehin <salehin24.rs@gmail.com>
2021-06-28 12:27:14 +0600

    userd model fixed and views added

--- models/users.model.js
+++ models/users.model.js
@@ -5,6 +5,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    gender:{
+        type: String,
+        required: true
+    },
     email:{
         type: String,
         unique: true,
