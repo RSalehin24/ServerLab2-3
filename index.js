a4a4d75586b72619035367db1a43c153e00c3396
Md. Reyanus Salehin <salehin24.rs@gmail.com>
2021-06-28 11:54:30 +0600

    database added

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 require('dotenv').config();
 const app = require('./app');
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT;
+const MongoDB = process.env.MongoDB;
+
+mongoose
+  .connect(MongoDB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Database Connected');
+  })
+  .catch((err) => {
+    if (err) {
+      console.log('Unable to connect to Mongoose');
+    }
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}. `);
